9d0210f22c281ffa412497981a8dbc614cc666a0
Mr-plastic <ppic5413@gmail.com>
2024-08-24 20:24:42 +0800

    first commit

--- views/js/script.js
+++ views/js/script.js
@@ -1,68 +1,160 @@
-const socket = io()
-const videogrid = document.getElementById('video-grid')
-const callBtn = document.getElementById('call')
-const myPeer = new Peer(undefined,{
-    host: '/',
-    port:'9000',
-    proxied: true,
-})
-const myVideo = document.createElement('video')
-myVideo.muted = true
+let socket = io.connect();
+let divVideoChatLobby = document.getElementById("video-chat-lobby");
+let divVideoChat = document.getElementById("video-chat-room");
+let joinButton = document.getElementById("join");
+let userVideo = document.getElementById("user-video");
+let peerVideo = document.getElementById("peer-video");
+let roomInput = document.getElementById("roomName");
+let roomName;
+let creator = false;
+let rtcPeerConnection;
+let userStream;
 
-const peers = {}
-navigator.mediaDevices.getUserMedia({   
-    video: true,
-    audio: true
-}).then(stream =>{
-    addVideoStream(myVideo,stream)
-    myPeer.on('call',call =>{
-        call.answer(stream)
-        const video = document.createElement('video')
-        call.on('stream',userVideoStream =>{
-            addVideoStream(video, userVideoStream)
-        })
-        call.on('error', err=> {
-            console.log('Call failed',err);
-        })
+// Contains the stun server URL we will be using.
+let iceServers = {
+  iceServers: [
+    { urls: "stun:stun.services.mozilla.com" },
+    { urls: "stun:stun.l.google.com:19302" },
+  ],
+};
+
+joinButton.addEventListener("click", function () {
+  if (roomInput.value == "") {
+    alert("Please enter a room name");
+  } else {
+    roomName = roomInput.value;
+    socket.emit("join", roomName);
+  }
+});
+
+// Triggered when a room is succesfully created.
+
+socket.on("created", function () {
+  creator = true;
+
+  navigator.mediaDevices
+    .getUserMedia({
+      audio: true,
+      video: { width: 1280, height: 720 },
     })
-
-    socket.on('user-connected', userId =>{
-        console.log(userId)
-        connectToNewUser(userId,stream)
+    .then(function (stream) {
+      /* use the stream */
+      userStream = stream;
+      divVideoChatLobby.style = "display:none";
+      userVideo.srcObject = stream;
+      userVideo.onloadedmetadata = function (e) {
+        userVideo.play();
+      };
     })
-})
+    .catch(function (err) {
+      /* handle the error */
+      alert("Couldn't Access User Media");
+    });
+});
 
-socket.on('user-disconnected', userId =>{
-    if(peers[userId]) peers[userId].close()
-})
+// Triggered when a room is succesfully joined.
 
-myPeer.on('open', id =>{
-    socket.emit('join-room', ROOM_ID, id)
-})
+socket.on("joined", function () {
+  creator = false;
 
-function connectToNewUser(userId, stream){
-    console.log("视频轨道数量:", stream.getVideoTracks().length);
-    console.log("音频轨道数量:", stream.getAudioTracks().length);
-    const call = myPeer.call(userId, stream)
-    const video = document.createElement('video')
-    // console.log("正在連接新用戶")
-    call.on('stream', userVideoStream =>{
-        console.log("正在連接新用戶")
-        addVideoStream(video, userVideoStream)
+  navigator.mediaDevices
+    .getUserMedia({
+      audio: true,
+      video: { width: 1280, height: 720 },
     })
-    call.on('close',() =>{
-        video.remove()
+    .then(function (stream) {
+      /* use the stream */
+      userStream = stream;
+      divVideoChatLobby.style = "display:none";
+      userVideo.srcObject = stream;
+      userVideo.onloadedmetadata = function (e) {
+        userVideo.play();
+      };
+      socket.emit("ready", roomName);
     })
-    peers[userId] = call
+    .catch(function (err) {
+      /* handle the error */
+      alert("Couldn't Access User Media");
+    });
+});
+
+// Triggered when a room is full (meaning has 2 people).
+
+socket.on("full", function () {
+  alert("Room is Full, Can't Join");
+});
+
+// Triggered when a peer has joined the room and ready to communicate.
+
+socket.on("ready", function () {
+  if (creator) {
+    rtcPeerConnection = new RTCPeerConnection(iceServers);
+    rtcPeerConnection.onicecandidate = OnIceCandidateFunction;
+    rtcPeerConnection.ontrack = OnTrackFunction;
+    rtcPeerConnection.addTrack(userStream.getTracks()[0], userStream);
+    rtcPeerConnection.addTrack(userStream.getTracks()[1], userStream);
+    rtcPeerConnection
+      .createOffer()
+      .then((offer) => {
+        rtcPeerConnection.setLocalDescription(offer);
+        socket.emit("offer", offer, roomName);
+      })
+
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+});
+
+// Triggered on receiving an ice candidate from the peer.
+
+socket.on("candidate", function (candidate) {
+  let icecandidate = new RTCIceCandidate(candidate);
+  rtcPeerConnection.addIceCandidate(icecandidate);
+});
+
+// Triggered on receiving an offer from the person who created the room.
+
+socket.on("offer", function (offer) {
+  if (!creator) {
+    rtcPeerConnection = new RTCPeerConnection(iceServers);
+    rtcPeerConnection.onicecandidate = OnIceCandidateFunction;
+    rtcPeerConnection.ontrack = OnTrackFunction;
+    rtcPeerConnection.addTrack(userStream.getTracks()[0], userStream);
+    rtcPeerConnection.addTrack(userStream.getTracks()[1], userStream);
+    rtcPeerConnection.setRemoteDescription(offer);
+    rtcPeerConnection
+      .createAnswer()
+      .then((answer) => {
+        rtcPeerConnection.setLocalDescription(answer);
+        socket.emit("answer", answer, roomName);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+});
+
+// Triggered on receiving an answer from the person who joined the room.
+
+socket.on("answer", function (answer) {
+  rtcPeerConnection.setRemoteDescription(answer);
+});
+
+// Implementing the OnIceCandidateFunction which is part of the RTCPeerConnection Interface.
+
+function OnIceCandidateFunction(event) {
+  console.log("Candidate");
+  if (event.candidate) {
+    socket.emit("candidate", event.candidate, roomName);
+  }
 }
 
-function addVideoStream(video,stream){
-    video.srcObject = stream
-    video.addEventListener('loadedmetadata', () =>{
-        video.play()
-    })
-    videogrid.append(video)
+// Implementing the OnTrackFunction which is part of the RTCPeerConnection Interface.
+
+function OnTrackFunction(event) {
+  peerVideo.srcObject = event.streams[0];
+  peerVideo.onloadedmetadata = function (e) {
+    peerVideo.play();
+  };
 }
-
-// callBtn.addEventListener('click',);
-
